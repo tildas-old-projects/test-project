f3c4073b82ecc93d85001a611af40a691c7d9196
xshotD <hi@youneeda.pw>
2016-10-10 00:38:16 +0000

    Major update

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	@gcc hello.c -o ./bin/test
+	@echo "Done."
+	@echo "Executable location - bin/test"
--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
+/* (c) 2016 xsd (S Stewart) under the GNU GPL v3 or later. */
 #include <stdio.h>
 int main() {
    /* I just copy pasted this. */
    printf("Hello! \n");
-  sprintf(cmd, "/bin/cat README");
+	/* Did you know you could do this? */
+  char cmd[] = "/bin/cat README";
   system(cmd);   
   return 0;
 }
