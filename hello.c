532c85468d96e1d96b93090d5d8df0962ac16e95
xshotD <hi@youneeda.pw>
2016-10-11 00:16:23 +0000

    extra useless comment.

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 /* (c) 2016 xsd (S Stewart) under the GNU GPL v3 or later. */
+/* This is a test - for real projects visit the GitHub. kthx */
 #include <stdio.h>
 int main() {
    /* I just copy pasted this. */
